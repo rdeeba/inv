f5aad7d317aaed6e96a1db89b96730b590607529
rdeeba <rdeeba@murex.com>
2018-03-24 17:34:39 +0200

    Initial commit

---
+++ database/src/main/java/com/deeba/database/Main.java
@@ -0,0 +1,14 @@
+package com.deeba.database;
+
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class Main {
+  private static final Logger LOG = getLogger(Main.class);
+
+  public static void main(String[] args) {
+    LOG.info("Ahla w sahla!");
+    System.out.println("Ya ahla w sahla");
+  }
+}
